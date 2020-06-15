b8519877193e7fc3055b08a2a68b6371181b382d
averne <averne381@gmail.com>
2020-06-15 12:55:09 +0200

    Update gitignore

--- push.sh
+++ push.sh
@@ -5,6 +5,6 @@ python3 organize.py
 if [ $? -eq 0 ]; then
     export GIT_SSH_COMMAND='ssh -i id_rsa'
     git add '*.log'
-    git commit -m 'Add new log files'
+    git commit -m "Add log file for $(date +%D)"
     git push
 fi
