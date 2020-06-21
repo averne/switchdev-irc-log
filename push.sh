0777464cada82157e251d1d1aa274ab051acd7ad
averne <averne381@gmail.com>
2020-06-22 00:00:02 +0200

    Add log files as of 06/22/20

--- push.sh
+++ push.sh
@@ -5,6 +5,6 @@ python3 organize.py
 if [ $? -eq 0 ]; then
     export GIT_SSH_COMMAND='ssh -i id_rsa'
     git add '*.log'
-    git commit -m "Add log file for $(date +%D)"
+    git commit -m "Add log files as of $(date +%D)"
     git push
 fi
