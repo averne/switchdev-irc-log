8d80a683bb833729cd1de07be5b66102bc101290
averne <averne381@gmail.com>
2021-04-04 16:30:00 +0200

    Strip IPs

--- organize.py
+++ organize.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import os, sys
+import os, sys, re
 from glob import glob
 from pathlib import Path
 
@@ -8,6 +8,15 @@ def main(argc, argv):
     logs = glob("*-*-*.log")
 
     for f in logs:
+        # Strip IPs
+        with open(f, "r+") as fp:
+            out = ""
+            for l in fp:
+                out += re.sub(r"(Quits|Joins|Parts): (\S+?) \(.*?@.*?\)", r"\1: \2", l)
+            fp.seek(0)
+            fp.truncate()
+            fp.write(out)
+
         old_path = Path(f)
         new_path = Path("/".join(old_path.stem.split("-")) + ".log")
         print(f"Moving {old_path} to {new_path}")
